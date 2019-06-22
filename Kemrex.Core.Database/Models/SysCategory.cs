7e427123efe5c63c44491048d9231505fc24e3ba
Piyaphon\Ton Chilling <t.kimji@gmail.com>
2019-06-22 21:44:56 +0700

    update transer table
    update transer table

--- Kemrex.Core.Database/Models/SysCategory.cs
+++ Kemrex.Core.Database/Models/SysCategory.cs
@@ -7,7 +7,12 @@ namespace Kemrex.Core.Database.Models
     {
         public SysCategory()
         {
+            TblJobOrderAttachmentType = new HashSet<TblJobOrderAttachmentType>();
+            TblJobOrderEquipmentType = new HashSet<TblJobOrderEquipmentType>();
+            TblJobOrderLandType = new HashSet<TblJobOrderLandType>();
+            TblJobOrderObstructionType = new HashSet<TblJobOrderObstructionType>();
             TblJobOrderProjectType = new HashSet<TblJobOrderProjectType>();
+            TblJobOrderUndergroundType = new HashSet<TblJobOrderUndergroundType>();
         }
 
         public int CategoryId { get; set; }
@@ -15,6 +20,11 @@ namespace Kemrex.Core.Database.Models
         public string CategoryName { get; set; }
 
         public virtual SysCategoryType CategoryType { get; set; }
+        public virtual ICollection<TblJobOrderAttachmentType> TblJobOrderAttachmentType { get; set; }
+        public virtual ICollection<TblJobOrderEquipmentType> TblJobOrderEquipmentType { get; set; }
+        public virtual ICollection<TblJobOrderLandType> TblJobOrderLandType { get; set; }
+        public virtual ICollection<TblJobOrderObstructionType> TblJobOrderObstructionType { get; set; }
         public virtual ICollection<TblJobOrderProjectType> TblJobOrderProjectType { get; set; }
+        public virtual ICollection<TblJobOrderUndergroundType> TblJobOrderUndergroundType { get; set; }
     }
 }
---
+++ Kemrex.Core.Database/Models/TblJobOrderLandType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kemrex.Core.Database.Models
+{
+    public partial class TblJobOrderLandType
+    {
+        public int JobOrderId { get; set; }
+        public int LandTypeId { get; set; }
+        public string Caption { get; set; }
+
+        public virtual SysCategory LandType { get; set; }
+    }
+}
