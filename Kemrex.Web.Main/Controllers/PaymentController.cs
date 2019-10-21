90d1c902d2595cf45980218211054f6be4bd8e77
Pokz739 <elastic.pok@gmail.com>
2019-10-21 21:52:53 +0700

    Update PaymentController.cs

--- Kemrex.Web.Main/Controllers/PaymentController.cs
+++ Kemrex.Web.Main/Controllers/PaymentController.cs
@@ -136,8 +136,8 @@ namespace Kemrex.Web.Main.Controllers
             if (Request.Form["PaymentDate"].ToString().Count() > 0)
             {
                 var dd = Request.Form["PaymentDate"].Split(' ')[0];
-                DateTime dateEng = Convert.ToDateTime(dd, _cultureEngInfo);
-                obj.PaymentDate = dateEng;
+                //DateTime dateEng = Convert.ToDateTime(dd, _cultureEngInfo);
+                //obj.PaymentDate = dateEng;
                 obj.StrPaymentDate = dd;
             }
             var AcctReceiveId = Request.Form["radio_bankaccount"];
